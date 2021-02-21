094c32d8442ec30a5318ae508fd97e6bb212fddf
MugiChu <mugichu29@gmail.com>
2021-02-22 02:26:27 +0700

    nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from typing import Optional
+import uvicorn
 
 app = FastAPI()
 
@@ -9,6 +9,5 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
